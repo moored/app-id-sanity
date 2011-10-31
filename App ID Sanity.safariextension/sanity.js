f6c71f7a9b3256b4cf44a0601760f9b77b98dc61
Simon Whitaker <simon@goosoftware.co.uk>
2011-10-31 20:36:49 +0000

    Add ability to edit the description of an App ID

--- App ID Sanity.safariextension/sanity.js
+++ App ID Sanity.safariextension/sanity.js
@@ -1,20 +1,31 @@
 var STATUS_ACTIVE = 'active';
 var STATUS_INACTIVE = 'inactive';
 
+var STORAGE_KEY_STATUS = 'status';
+var STORAGE_KEY_PSEUDONYM = 'pseudonym';
+
 function BundleId(bundleId) {
     this.bundleId = bundleId;
 }
 
-BundleId.prototype.isInactive = function() {
-    return localStorage.getItem(this.getActivityStatusKey()) == STATUS_INACTIVE;
+BundleId.prototype.getStorageKey = function(keyName) {
+    return 'uk.co.goosoftware.app-id-sanity/' + this.bundleId + '/' + keyName;
 }
 
-BundleId.prototype.getActivityStatusKey = function() {
-    return 'uk.co.goosoftware.app-id-sanity/' + this.bundleId + '/status';
+BundleId.prototype.isInactive = function() {
+    return localStorage.getItem(this.getStorageKey(STORAGE_KEY_STATUS)) == STATUS_INACTIVE;
 }
 
 BundleId.prototype.setActive = function(isActive) {
-    localStorage.setItem(this.getActivityStatusKey(), isActive ? STATUS_ACTIVE : STATUS_INACTIVE);
+    localStorage.setItem(this.getStorageKey(STORAGE_KEY_STATUS), isActive ? STATUS_ACTIVE : STATUS_INACTIVE);
+}
+
+BundleId.prototype.getPseudonym = function() {
+    return localStorage.getItem(this.getStorageKey(STORAGE_KEY_PSEUDONYM));
+}
+
+BundleId.prototype.setPseudonym = function(pseudonym) {
+    localStorage.setItem(this.getStorageKey(STORAGE_KEY_PSEUDONYM), pseudonym);
 }
 
 if (document.location.href.match('/manage/bundles/index.action$')) {
@@ -32,9 +43,24 @@ if (document.location.href.match('/manage/bundles/index.action$')) {
                 if (bundleId.isInactive()) {
                     $(tr).addClass('inactive');
                 }
+                
+                // Make the description editable
+                var name_td = $(tr).find('td.name');
+                var re = /<br>(.+)/;
+                name_td.html(name_td.html().replace(re, '<br><input type="text" class="description" value="$1"></input>'));
+                
+                var description = name_td.find('input.description');
+                var pseudonym = bundleId.getPseudonym();
+                if (pseudonym && pseudonym.length > 0) {
+                    description.val(pseudonym);
+                };
+                
+                // Update the bundleId pseudonym as the user changes it
+                description.bind('paste cut keyup mouseup change', function(){
+                    bundleId.setPseudonym(description.val());
+                });
 
-                var delete_td = $('<td style="vertical-align:middle; text-align:center;"></td>');
-
+                // Create the active/inactive checkbox
                 var checkbox = $('<input type="checkbox"></input>');
                 checkbox.prop('checked', !bundleId.isInactive());
                 checkbox.click(function(event) {
@@ -42,6 +68,9 @@ if (document.location.href.match('/manage/bundles/index.action$')) {
                     bundleId.setActive(is_active);
                     $(tr).toggleClass('inactive', !is_active);
                 });
+
+                // Create a new <td> to store the active/inactive checkbox
+                var delete_td = $('<td style="vertical-align:middle; text-align:center;"></td>');
                 
                 // Append checkbox to td, appent td to tr
                 delete_td.append(checkbox);
@@ -60,5 +89,10 @@ if (document.location.href.match('/manage/bundles/index.action$')) {
         if (bundleId.isInactive()) {
             $(option).remove();
         }
+        var pseudonym = bundleId.getPseudonym();
+        if (pseudonym && pseudonym.length > 0) {
+            $(option).html(pseudonym);
+        };
     });
+    
 }
